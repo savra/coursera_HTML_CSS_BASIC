07516af39004a0d7a3b0f3d9a1a0199aeb5ab51c
Sergey Savrukhin <savrawar@mail.ru>
2021-08-14 18:21:06 +0300

    fix second task

--- src/javascript_part1/secondWeek/unique-hashtags-list/index.js
+++ src/javascript_part1/secondWeek/unique-hashtags-list/index.js
@@ -3,23 +3,12 @@
  * @returns {String}
  */
 module.exports = function (hashtags) {
-    if (hashtags.length === 0) {
+    if(hashtags.length === 0){
         return "";
-    } else {
-        for (let i = 0; i < hashtags.length - 1; i++) {
-            if (typeof hashtags[i] !== "string") {
-                continue;
-            }
-
-            hashtags[i] = hashtags[i].toLowerCase();
-            for (let j = i + 1; j < hashtags.length; j++) {
-                if (hashtags[i].toLowerCase() === hashtags[j].toLowerCase()) {
-                    hashtags.splice(j, 1);
-                    j--;
-                }
-            }
-        }
     }
-
-    return hashtags.join(", ");
+    hashtags = hashtags.join(" ").toLowerCase().split(" ");
+    let new_hashtags = hashtags.filter((item, index) => {
+        return hashtags.indexOf(item) === index;
+    })
+    return new_hashtags.join(', ');
 };
--- src/javascript_part1/secondWeek/unique-hashtags-list/test.js
+++ src/javascript_part1/secondWeek/unique-hashtags-list/test.js
@@ -1,4 +1,4 @@
-let sourceArray = ['a', 'B', 'b', 'b', 'B', 'B', 'B', "B", "B", "B", "B"];
+let sourceArray = [1, 'B', 'b', 1, ".", 'a', 'a b', 'с', "B", "с", "B", "с", "a", "a", "a", "a b", "a b", "jkhkhk", "lklk n", "k"];
 let sourceArray3 = ["kkk", "kKk"];
 let sourceArray4 = [];
 
@@ -6,15 +6,19 @@ if (sourceArray.length === 0) {
     console.log("");
 } else {
     for (let i = 0; i < sourceArray.length - 1; i++) {
+        if ((typeof sourceArray[i]) !== "string" || (sourceArray[i].indexOf(" ") !== -1)) {
+            sourceArray.splice(i, 1);
+            i--;
+            continue;
+        }
+
         sourceArray[i] = sourceArray[i].toLowerCase();
         for (let j = i + 1; j < sourceArray.length; j++) {
             console.log("i = " + i);
             console.log("j = " + j);
-            if (sourceArray[i].toLowerCase() === sourceArray[j].toLowerCase()) {
-                console.log("before delete" + " " +  sourceArray);
+            if ((typeof sourceArray[j]) !== "string" || (sourceArray[j].indexOf(" ") !== -1) || sourceArray[i].toLowerCase() === sourceArray[j].toLowerCase()) {
                 sourceArray.splice(j, 1);
                 j--;
-                console.log("after delete" + " " +  sourceArray);
             }
         }
     }
