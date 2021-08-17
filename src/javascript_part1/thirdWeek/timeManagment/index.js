ec169634116e41ee0dcbc5fddb38e546ad696c9f
Sergey Savrukhin <savrawar@mail.ru>
2021-08-17 19:21:31 +0300

    create methods add and sub

--- src/javascript_part1/thirdWeek/timeManagment/index.js
+++ src/javascript_part1/thirdWeek/timeManagment/index.js
@@ -4,7 +4,33 @@
  */
 module.exports = function (date) {
     let timeManager = {
-        _date : new Date()
+        _date: new Date(),
+        add: function (value, unit) {
+            this.validate(value, unit);
+        },
+        subtract : function (value, unit) {
+            this.validate(value, unit);
+        },
+        validate: function (value, unit) {
+            if (value < 0) {
+                return new TypeError("Количество единиц не может быть отрицательным");
+            }
+
+            if (!/years|months|days|hours|minutes/i.test(unit)) {
+                return new TypeError("Неизвестная единица измерения");
+            }
+        }
     };
 
+    Object.defineProperty(timeManager, "date", {
+        get: function () {
+            return this._date;
+        },
+        set: function (value) {
+            this._date = value;
+        }
+    });
+
+    let normalizeDate = date.match(/(\d{4,})-([0-1][0-2])-([1-9]|1[0-2])/);
+    return normalizeDate;
 };
--- src/javascript_part1/thirdWeek/timeManagment/test.js
+++ src/javascript_part1/thirdWeek/timeManagment/test.js
@@ -1,21 +1,8 @@
 // Подключаем свою функцию
-//var date = require('./index.js');
+var date = require('./index.js');
 
 //date("2017-05-16 13:45");
 
+let sourceDate = "2017-05-16 13:45";
 
-var timeManager = {
-    _date: new Date()
-};
-
-Object.defineProperty(timeManager, "date", {
-    get: function () {
-        return this._date;
-    },
-    set: function (value) {
-        this._date = value;
-    }
-});
-
-
-console.log(timeManager.date);
+console.log(date(sourceDate));
