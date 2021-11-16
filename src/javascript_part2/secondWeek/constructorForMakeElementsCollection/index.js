a35e97e4a64793686bca1a1abadab3af6678f08d
Sergey Savrukhin <savrawar@mail.ru>
2021-11-16 21:50:16 +0300

    preparation for the task

---
+++ src/javascript_part2/secondWeek/constructorForMakeElementsCollection/index.js
@@ -0,0 +1,26 @@
+module.exports = Collection;
+
+/**
+ * Конструктор коллекции
+ * @constructor
+ */
+function Collection() {}
+
+
+// Методы коллекции
+Collection.prototype.values = function () {};
+Collection.prototype.at = function () {};
+Collection.prototype.append = function () {};
+Collection.prototype.removeAt = function () {};
+Collection.prototype.count = function () {};
+// другие методы
+
+
+/**
+ * Создание коллекции из массива значений
+ */
+Collection.from = function () {};
+
+
+var collection = new Collection();
+collection.count(); // 0
