7d7abba70b87c9be61479a7232ba06ff0d53107e
Sergey Savrukhin <savrawar@mail.ru>
2021-11-29 22:36:59 +0300

    new task

--- src/javascript_part2/secondWeek/constructorForMakeElementsCollection/index.js
+++ src/javascript_part2/secondWeek/constructorForMakeElementsCollection/index.js
@@ -4,19 +4,46 @@ module.exports = Collection;
  * Конструктор коллекции
  * @constructor
  */
-function Collection() {}
+function Collection() {
+    this.collection = Array.from([]);
+}
 
 
 // Методы коллекции
-Collection.prototype.values = function () {};
-Collection.prototype.at = function () {};
-Collection.prototype.append = function () {};
-Collection.prototype.removeAt = function () {};
-Collection.prototype.count = function () {};
+Collection.prototype.values = function () {
+    return this.collection;
+};
+Collection.prototype.at = function (index) {
+    return this.collection[index - 1];
+};
+Collection.prototype.append = function (value) {
+    if (value instanceof Collection) {
+        Array.from(value.collection).forEach(elem => this.collection.push(elem));
+    } else if (value instanceof Array) {
+        value.forEach(elem => this.collection.push(elem));
+    } else {
+        this.collection.push(value);
+    }
+};
+Collection.prototype.removeAt = function (index) {
+    if (this.at(index)) {
+        this.collection.splice(index - 1, 1);
+        return true;
+    } else {
+        return false;
+    }
+};
+Collection.prototype.count = function () {
+    return this.collection.length;
+};
 // другие методы
 
 
 /**
  * Создание коллекции из массива значений
  */
-Collection.from = function () {};
+Collection.from = function (array) {
+    let collection = new Collection();
+    collection.append(array);
+    return collection;
+};
