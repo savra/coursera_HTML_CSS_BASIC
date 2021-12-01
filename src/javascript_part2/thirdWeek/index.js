d55ae5391dd466c29eac53a0cd169a111dab385f
Sergey Savrukhin <savrawar@mail.ru>
2021-12-01 21:24:27 +0300

    completed task

--- src/javascript_part2/thirdWeek/index.js
+++ src/javascript_part2/thirdWeek/index.js
@@ -3,5 +3,21 @@
  * @param {Function} callback
  */
 module.exports = function (operations, callback) {
+    let promises = [];
 
+
+
+    for (let operation of operations) {
+        promises.push(new Promise((resolve, reject) => {
+            operation((err, data) => {
+                if (!err) {
+                    resolve(data);
+                } else {
+                    reject(err);
+                }
+            });
+        }));
+    }
+
+    Promise.all(promises).then(data => callback(null, data)).catch(reason => callback(reason));
 };
